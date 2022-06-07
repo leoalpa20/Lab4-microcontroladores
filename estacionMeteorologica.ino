8ae7fbcee79eae87129df3dc152379e7dfb7ac7c
Leonardo Alfaro <leonardoalfaro@Leonardos-MacBook-Air.local>
2022-06-06 23:44:14 -0600

    code in progress

---
+++ estacionMeteorologica.ino
@@ -0,0 +1,8 @@
+#include <Adafruit_PCD8544.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <math.h>
+#include <stdio.h>
+#include <EEPROM.h>
+#include <Servo.h>
+#include "LowPower.h"
--- serialRead.py
+++ serialRead.py
@@ -1,1 +1,14 @@
-sakndansdnas
+import csv
+import time
+import paho.mqtt.client as mqtt
+import json
+import serial
+
+def on_connect(client, userdata, flags, connectionOkFlag):
+    if connectionOkFlag == 0:
+        client.connectionOk = True
+        print("Connection succesful")
+    else:
+        print("No connection, return code ", connectionOkFlag)
+        client.loop_stop()
+
