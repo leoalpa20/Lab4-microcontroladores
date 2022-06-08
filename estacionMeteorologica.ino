19088e13509bc19815c3361815005f5dbc5c4236
Leonardo Alfaro <leonardoalfaro@Leonardos-MacBook-Air.local>
2022-06-07 20:47:38 -0600

    added time variables

--- estacionMeteorologica.ino
+++ estacionMeteorologica.ino
@@ -14,6 +14,7 @@
 #define YAXISPOSITION 1
 #define DELTA 2
 #define RANDOMPOS 10
+#define TEMPERATURE_TOKEN "a9Ggq3euSFkiqrHOaraF"
 
 // LED pins and definition
 
@@ -50,4 +51,59 @@ float R1 = 100000;
 float logR2, R2, TEMP;
 // obtained from  https://www.thinksrs.com/downloads/programs/Therm%20Calc/NTCCalibrator/NTCcalculator.html
 float c1 = 0.8586139205e-03, c2 = 2.059709585e-04, c3 = 0.8130635267e-07;
+
+// Variables for the temperature
+
+int thermistorValue;
+int displayTemperature;
+
+// Variables for the humidity
+
+int displayHumidity;
+int humidityVale;
+float normalizedHumidity;
+
+// Variables for the wind
+
+int windValue;
+int displayWind;
+float windNormalized;
+
+// Variables for the rain
+
+float rainNormalized;
+int rainValue;
+int displayRain;
+
+// Variables for the battery
+
+float batteryNormalized;
+int batteryValue;
+int displayBattery;
+float batteryVoltage;
+
+// Variables for the light 
+
+int lightValue;
+float lightNormalized;
+int displayLight;
+// Values in K ohms
+const long darknessResistance = 500;
+const int resistanceToLight = 12.5;
+const int calibrationResistance = 10;
+const int pinLDR = A1;
+int lightSourceVoltage;
+int ilimunationVoltage;
+
+// Variables for keeping track of the time
+
+unsigned long startingTime;
+unsigned long currentTime;
+unsigned long generalCurrentTime;
+unsigned long generalStartingTime;
+const unsigned long tenMinutes = 600000; // Value in ms
+unsigned int startTimerEnabled = 1;
+
+// Variables to configure the servos
+
 
