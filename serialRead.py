e22ada990af538d00f4085471887c9f05df2eee7
Leonardo Alfaro <leonardoalfaro@Leonardos-MacBook-Air.local>
2022-06-12 23:43:21 -0600

    final code for the python file

--- serialRead.py
+++ serialRead.py
@@ -1,25 +1,67 @@
-import csv
+# Code to connect to the ThingsBoard server
+# Reads the data from the serial port, connects
+# to the server, sends the data in JSON format
+# and prints to the topic selected which can be 
+# seen in the website.
+
+import serial
 import time
 import paho.mqtt.client as mqtt
 import json
-import serial
 
-def on_connect(client, userdata, flags, connectionOkFlag):
-    if connectionOkFlag == 0:
-        client.connectionOk = True
-        print("Connection succesful")
+def on_connect(client, userdata, flags, return_code):
+    if return_code==0:
+        client.connectionOkFlag=True 
+        print("Connection succesful") 
     else:
-        print("No connection, return code ", connectionOkFlag)
-        client.loop_stop()
+        print("Incorrect connection = ",return_code) 
+        client.loop_stop()  
 
-def on_message( client, userdata, message):
-    print(message.topic + " " + str(message.payload))
-
+ 
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
 
 client = mqtt.Client('python3')
 client.on_connect = on_connect
 client.on_message = on_message
 
-mqtt.Client.connectionIsOk = False
-mqtt.Client.suppression = False
+mqtt.Client.connectionOkFlag = False 
+mqtt.Client.incorrectConnectionReturnError = False
+
+PuertoSerial = serial.Serial(port = '/tmp/ttyS1')
+
+
+# Token is from ThingsBoard.
+token = "VrUpeXsOmbW8qYeV69ED"
+topic = "v1/devices/me/telemetry"
+requestTopic = "v1/devices/me/attributes/request/1"
+
+client.username_pw_set(token, password=None)
+
+# Connect to the website 
+client.connect("iot.eie.ucr.ac.cr", 1883, 60)
+while not(client.connectionOkFlag):
+	client.loop()
+	time.sleep(1)
+time.sleep(3)
+
+
+dataToSend = dict()
+for i in range(10):
+	valueToSend = PuertoSerial.readline().decode().split(',')
+	
+	dataToSend["Battery (%)"]=valueToSend[0]
+	dataToSend["Temp (C)"]=valueToSend[1]
+	dataToSend["Humidity (%)"]=valueToSend[2]
+	dataToSend["Wind (m/s)"]=valueToSend[3]
+	dataToSend["Rain (mm)"]=valueToSend[4]
+	dataToSend["Light (lux)"]=valueToSend[5]
+	data_out = json.dumps(dataToSend)
+	
+	print(valueToSend)
+
+	client.publish(topic, data_out, 0)
+	time.sleep(5)
+	client.loop()
+client.disconnect()
 
